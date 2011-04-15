3a94005d3589fabb290576b37ac78ced92b40950
Yuhui H <eyecat_AT_gmail.com>
2011-04-15 12:23:00 -0700

    Compensate variables without __dict__ attribute

--- pycrash/stackframe.py
+++ pycrash/stackframe.py
@@ -36,7 +36,7 @@ del re
 class StackFrame(object):
 	def __init__(self, frame):
 		assert type(frame) is _FrameType, "1st parameter must be a Frame object"
-		
+
 		self.__frame = frame
 
 	def getArgCount(self):
@@ -49,7 +49,7 @@ class StackFrame(object):
 		filename = _replace(filename, "<", "(")
 		filename = _replace(filename, ">", ")")
 		return filename
-	
+
 	def getLineNumber(self):
 		# Coded by Marc-Andre Lemburg from the example of PyCode_Addr2Line()
 		# in compile.c.
@@ -58,7 +58,7 @@ class StackFrame(object):
 		c = self.__frame.f_code
 		if not hasattr(c, 'co_lnotab'):
 			return self.__frame.f_lineno
-			
+
 		tab = c.co_lnotab
 		line = c.co_firstlineno
 		stopat = self.__frame.f_lasti
@@ -69,13 +69,13 @@ class StackFrame(object):
 				break
 			line = line + ord(tab[i+1])
 		return line
-		
+
 	def getFrameName(self):
 		return str(self.__frame.f_code.co_name)
 
 	def getStackSize(self):
 		return self.__frame.f_code.co_stacksize
-	
+
 	def getVar(self, name):
 		assert name is not "", "1st parameter must be a string keyword"
 		return self.__frame.f_locals[name]
@@ -88,10 +88,10 @@ class StackFrame(object):
 		def getVarType(var): #Returns the type
 				regex = _compile("\'(.)+\'", _IGNORECASE)
 				varType = str(type(var))
-				
+
 				#skip <type 'XX'> or <class 'XX> text
 				search = regex.search(varType)
-				if search is not None: 
+				if search is not None:
 					return varType[search.start() + 1:search.end() - 1]
 				else:
 					return "(undefined)"
@@ -102,12 +102,12 @@ class StackFrame(object):
 			#Let's check if 'var' is an instance of a new-style class
 			regex = _compile("<class \'(.)+\'>", _IGNORECASE)
 			varType = str(type(var))
-				
+
 			search = regex.search(varType)
 			if search is not None:
-				return 1 
+				return 1
 			return 0
- 
+
 		#frame info
 		strXML = "\t\t<frame name=\"" + escape(self.getFrameName()) + "\""
 		strXML += " argcount=\"" + escape(str(self.getArgCount())) + "\""
@@ -118,42 +118,45 @@ class StackFrame(object):
 		#var info
 		#regex = re.compile("'[a-z]+'", re.IGNORECASE)
 		#regex = re.compile("<type (.)+>", re.IGNORECASE)
-		
+
 		for item in self.getVarNames():
 			var = None
 			try: #the variable can be undefined
 				var = self.getVar(item)
 				varValue = str(var)
 				varType = getVarType(var)
-				
+
 				#replacing '<' and '>' with '(' and ')'
 				#to avoid XML parser errors
 				varValue = _replace(varValue, "<", "(")
 				varValue = _replace(varValue, ">", ")")
-				
+
 			except:
 				varValue = "(undefined)"
 				varType = "(undefined)"
-			
+
 			strXML += "\t\t\t<var name=\"" + escape(item) + "\""
 			strXML += " type=\"" + escape(varType) + "\">" + escape(varValue)
-			
+
 			if isInstance(var): #Add all object attributes
-				attrKeys = var.__dict__.keys()
-				if attrKeys:
-					del attrKeys[0] #Let's skip 1st element to avoid redundant info
-				for attr in attrKeys:
-					varType = getVarType(var.__dict__[attr])
-				
-					strXML += "\n\t\t\t\t<attr name=\"" + escape(attr )
-					strXML += "\" type=\"" + escape(varType) + "\">"
-					attrValue =  str(var.__dict__[attr])
-					attrValue = _replace(attrValue, "<", "(")
-					attrValue = _replace(attrValue, ">", ")")
-					strXML += escape(attrValue) + "</attr>"
+				try:
+					attrKeys = var.__dict__.keys()
+					if attrKeys:
+						del attrKeys[0] #Let's skip 1st element to avoid redundant info
+					for attr in attrKeys:
+						varType = getVarType(var.__dict__[attr])
+
+						strXML += "\n\t\t\t\t<attr name=\"" + escape(attr )
+						strXML += "\" type=\"" + escape(varType) + "\">"
+						attrValue =  str(var.__dict__[attr])
+						attrValue = _replace(attrValue, "<", "(")
+						attrValue = _replace(attrValue, ">", ")")
+						strXML += escape(attrValue) + "</attr>"
+				except AttributeError:
+					pass
 				strXML += "\n\t\t\t</var>\n"
 			else:
 				strXML += "</var>\n"
-			
+
 		strXML += "\t\t</frame>\n"
 		return strXML
